ecc1f81ce14993335209cbec13a309fbc05a10a3
Júlio Dias <julio.sousa0j4@gmail.com>
2024-10-26 15:41:32 -0300

    fix: correção de bugs

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function valedateEmails(){
   var email = document.getElementById("email").value;
   var ComfirEmail = document.getElementById('ComfirEmail').value; 
   var MsgError = document.getElementById('MsgError'); 
+  var msgSuccess = document.getElementById('msg-Success'); 
 
   if (email !== ComfirEmail) {
     MsgError.style.display = 'inline'; 
@@ -17,13 +18,30 @@ function valedateEmails(){
   }
   else {
     MsgError.style.display = 'none'
+    msgSuccess.style.display = 'inline'
     return true; 
   }
 }
-document.getElementById('email').addEventListener('input', function(){
-    document.getElementById('MsgError').style.display = 'none'; 
-})
 
-document.getElementById('ComfirEmail').addEventListener(input, function(){
-    document.getElementById('MsgError').style.display = 'none';
-}); 
+document.getElementById("submit-form").addEventListener('click', function(event) {
+  event.preventDefault(); 
+  
+  if (valedateEmails())  {
+    document.getElementById('email-Form').valreset(); 
+  }
+});
+
+document.querySelectorAll("#email-Form").forEach(input => {
+  input.addEventListener("input", function() {
+    document.getElementById("msg-Success").style.display = 'none';
+  });
+
+  document.getElementById('email').addEventListener('input', function() {
+      document.getElementById('MsgError').style.display = 'none'; 
+  });
+  
+  document.getElementById('ConfirEmail').addEventListener('input', function() {
+      document.getElementById('ConfirEmail').style.display = 'none'; 
+  })
+
+});
